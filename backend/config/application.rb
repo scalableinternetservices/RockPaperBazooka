5303bf940e8f4cb974966b68f21c72495a94d249
Scott Chow <tehchowster@gmail.com>
2019-10-30 20:02:17 -0700

    sc - rename backend app and created frontend app

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -16,10 +16,10 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Backend
+module RockPaperBazooka
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
