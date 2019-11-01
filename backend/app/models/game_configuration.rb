90fa3358aea5655b3d0a657fe89de69e1a21049d
Andrew Thompson <andrewdt23@gmail.com>
2019-11-01 14:07:52 -0700

    at/dh - generate game_configuration and user scaffolds

---
+++ backend/app/models/game_configuration.rb
@@ -0,0 +1,2 @@
+class GameConfiguration < ApplicationRecord
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_01_210342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "configurations", force: :cascade do |t|
+    t.string "name"
+    t.integer "num_matches"
+    t.string "input_set"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "game_configurations", force: :cascade do |t|
+    t.string "name"
+    t.integer "num_matches"
+    t.string "input_set"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
