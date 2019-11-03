368c80f0b5c2821ef71f521b26b4bfd947b4ed40
Scott Chow <tehchowster@gmail.com>
2019-11-03 09:35:26 -0800

    sc - sketched the match configuration form and show

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,15 @@ import GameConfigurationForm from './Components/GameConfiguration/Form';
 import GameConfiguration from './Components/GameConfiguration/Show';
 import UserForm from './Components/User/Form';
 import User from './Components/User/Show';
+import MatchForm from './Components/Match/Form';
+import Match from './Components/Match/Show';
 function App() {
   return (
     <div className='App'>
+      <Match match={{ game_configuration_id: 0 }} />
+      <MatchForm /> <br />
       <GameConfiguration gameConfiguration={{}} />
-      <GameConfigurationForm />
+      <GameConfigurationForm /> <br />
       <User user={{ name: 'Scotty McGee' }} />
       <UserForm />
     </div>
---
+++ frontend/src/Components/Match/Form.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Form, Button, Input } from 'reactstrap';
+
+class MatchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { game_configuration_id: 0 };
+  }
+
+  onChange = e => {
+    if (e.target.type === 'number') {
+      this.setState({ [e.target.name]: parseInt(e.target.value) });
+    } else {
+      this.setState({ [e.target.name]: e.target.value });
+    }
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    // this.props.onSubmit();
+  };
+
+  render() {
+    let configurations = [
+      { id: 0, name: 'Rock Paper Scissors' },
+      { id: 1, name: 'Rock Paper Bazooka' },
+    ];
+
+    let makeOption = configuration => {
+      return <option value={configuration.id}>{configuration.name}</option>;
+    };
+
+    return (
+      <Form onSubmit={this.onSubmit}>
+        <Input
+          name='game_configuration_id'
+          type='select'
+          onChange={this.onChange}
+        >
+          {configurations.map(makeOption)}
+        </Input>
+        <br />
+        <Button type='submit' color='primary'>
+          Submit
+        </Button>
+      </Form>
+    );
+  }
+}
+
+export default MatchForm;
