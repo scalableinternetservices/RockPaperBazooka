3f5417a9d74601d4c98fe417845fbaafc7da4c96
Andrew Thompson <andrewdt23@gmail.com>
2019-11-05 15:54:13 -0800

    at - add patch /matches/:id/join endpoint

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :matches
+  resources :matches do
+    patch 'join', on: :member
+  end
   resources :users
   resources :game_configurations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
