6abf0cbb51f07b151b4b549fd253bed84bdeb933
Andrew Thompson <andrewdt23@gmail.com>
2019-11-06 08:37:28 -0800

    at - add seed for default rock paper scissors configuration

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+default_configuration = GameConfiguration.create({name: "Rock, Paper, Scissors", num_matches: 3, input_set: "rock paper scissors"})
