83cb56e04e0c3215c6f7cd727b2147ebe67fbb7a
Andrew Thompson <andrewdt23@gmail.com>
2019-11-12 16:01:22 -0800

    at - add ListGroup bootstrap to messages show

--- frontend/src/Components/Messages/Show.jsx
+++ frontend/src/Components/Messages/Show.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 function Show() {
   let messages = ['message 1', 'message 2', 'message 3', 'message 4'];
   return (
     <div>
-      {messages.map(message => <p>{message}</p>)}
+      <ListGroup>
+        <ListGroupItem color="primary">Messages</ListGroupItem>
+            {messages.map((message, index) => {
+                const color = index%2 === 0 ? 'light' : 'dark';
+                return (<ListGroupItem color={color}> {message} </ListGroupItem>);
+            })}
+      </ListGroup>
     </div>
   );
 }
