41627fc8bfb2e9f159638ac6e9cbc211c1fa6733
Andrew Thompson <andrewdt23@gmail.com>
2019-11-12 20:56:37 -0800

    at - connect messages show to client

--- frontend/src/Components/Messages/Show.jsx
+++ frontend/src/Components/Messages/Show.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import { Badge, ListGroup, ListGroupItem } from 'reactstrap';
+import Client from "../../Clients/Client";
 
-function Show() {
-  let messages = [
-      {content: 'message 1', user_id: 1},
-      {content: 'message 2', user_id: 1},
-      {content: 'message 3', user_id: 2},
-      {content: 'message 4', user_id: 1}
-  ];
-  const user_ids = [...new Set(messages.map(message => message.user_id))];
-  return (
-    <div className="test">
-      <ListGroup>
-        <ListGroupItem color="primary">Messages</ListGroupItem>
-            {messages.map((message, index) => {
-                const color = user_ids[0] === message.user_id ? 'light' : 'dark';
-                return (<ListGroupItem style={{color: 'black'}} color={color}> <Badge>{message.user_id}</Badge>: {message.content} </ListGroupItem>);
-            })}
-      </ListGroup>
-    </div>
-  );
+class Messages extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+<<<<<<< HEAD
+        id: 1,
+        messages: []
+=======
+        id: this.props.id,
+        messages: [],
+        users: new Map()
+>>>>>>> bacc752... fixup connect messages to client
+    };
+  }
+
+  componentDidMount() {
+      Client.messages(this.state.id)
+        .then(response => {
+            console.log(response);
+            this.setState({ messages: response.data });
+        })
+        .catch(console.log);
+  }
+
+  render() {
+    const user_ids = [...new Set(this.state.messages.map(message => message.user_id))];
+    return (
+        <div className="test">
+          <ListGroup>
+            <ListGroupItem color="primary">Messages</ListGroupItem>
+                {this.state.messages.map((message, index) => {
+                    const color = user_ids[0] === message.user_id ? 'light' : 'dark';
+                    return (<ListGroupItem key={message.id} style={{color: 'black'}} color={color}> <Badge>{message.user_id}</Badge>: {message.content} </ListGroupItem>);
+                })}
+          </ListGroup>
+        </div>
+    );
+  }
 }
 
-export default Show;
+export default Messages;
