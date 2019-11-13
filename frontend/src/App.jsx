f2f6ca1a501a3834a3dddb913ddbf2291e06d35c
Andrew Thompson <andrewdt23@gmail.com>
2019-11-12 21:42:28 -0800

    at - show messaging on main page for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,12 @@ import UserForm from "./Components/User/Form";
 import User from "./Components/User/Show";
 import MatchForm from "./Components/Match/Form";
 import Match from "./Components/Match/Show";
+<<<<<<< HEAD
 import MatchTable from "./Components/MatchTable/MatchTable"
+=======
+import Messages from "./Components/Messages/Show";
+import MessageForm from "./Components/Messages/Form";
+>>>>>>> at - show messaging on main page for now
 
 function App() {
   return (
@@ -14,6 +19,8 @@ function App() {
       <MatchTable />
       <Match match={{ game_configuration_id: 0 }} />
       <MatchForm /> <br />
+      <Messages id={1} />
+      <MessageForm match_id={1} user_id={1} /> <br />
       <GameConfiguration gameConfiguration={{}} />
       <GameConfigurationForm /> <br />
       <User user={{ name: "Scotty McGee" }} />
