1ac46061f36d61fdad736375ea8b0da3a462adc3
Andrew Thompson <andrewdt23@gmail.com>
2019-11-12 16:28:03 -0800

    at - update message list to show author and unique colors

--- frontend/src/Components/Messages/Show.jsx
+++ frontend/src/Components/Messages/Show.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Badge, ListGroup, ListGroupItem } from 'reactstrap';
 
 function Show() {
-  let messages = ['message 1', 'message 2', 'message 3', 'message 4'];
+  let messages = [
+      {content: 'message 1', user_id: 1},
+      {content: 'message 2', user_id: 1},
+      {content: 'message 3', user_id: 2},
+      {content: 'message 4', user_id: 1}
+  ];
+  const user_ids = [...new Set(messages.map(message => message.user_id))];
   return (
-    <div>
+    <div className="test">
       <ListGroup>
         <ListGroupItem color="primary">Messages</ListGroupItem>
             {messages.map((message, index) => {
-                const color = index%2 === 0 ? 'light' : 'dark';
-                return (<ListGroupItem color={color}> {message} </ListGroupItem>);
+                const color = user_ids[0] === message.user_id ? 'light' : 'dark';
+                return (<ListGroupItem style={{color: 'black'}} color={color}> <Badge>{message.user_id}</Badge>: {message.content} </ListGroupItem>);
             })}
       </ListGroup>
     </div>
