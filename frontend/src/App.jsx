c320225d2f3f8cba0019bc37e4c4ddef90badc25
Andrew Thompson <andrewdt23@gmail.com>
2019-11-14 15:21:09 -0800

    at - add react-router to frontend and setup initial routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,20 +9,37 @@ import Match from "./Components/Match/Show";
 import MatchTable from "./Components/MatchTable/MatchTable"
 import Messages from "./Components/Messages/Show";
 import MessageForm from "./Components/Messages/Form";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <MatchTable />
-      <Match match={{ game_configuration_id: 0 }} />
-      <MatchForm /> <br />
-      <Messages id={1} />
-      <MessageForm match_id={1} user_id={1} /> <br />
-      <GameConfiguration gameConfiguration={{}} />
-      <GameConfigurationForm /> <br />
-      <User user={{ name: "Scotty McGee" }} />
-      <UserForm />
-    </div>
+    <Router>
+        <div className="App">
+            <Switch>
+                <Route path="/configuration">
+                    <GameConfiguration gameConfiguration={{}} />
+                    <GameConfigurationForm /> <br />
+                </Route>
+                <Route path="/messages">
+                    <Messages id={1} />
+                    <MessageForm match_id={1} user_id={1} /> <br />
+                </Route>
+                <Route path="/match">
+                    <Match match={{ game_configuration_id: 0 }} />
+                    <MatchForm /> <br />
+                </Route>
+                <Route path="/">
+                    <User user={{ name: "Scotty McGee" }} />
+                    <UserForm />
+                </Route>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
