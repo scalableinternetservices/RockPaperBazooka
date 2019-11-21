def7dfe35570534174dfbf7e62d2489232e56be8
Andrew Thompson <andrewdt23@gmail.com>
2019-11-20 18:00:17 -0800

    at - add links to header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import "./Header.css";
 import {
   Navbar,
+  NavbarBrand,
   Nav,
   NavItem
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
 
@@ -12,9 +14,17 @@ class Header extends React.Component {
       return (
         <div className="header">
           <Navbar color="light" light expand="md">
-              <Nav className="mx-auto" navbar>
+            <NavbarBrand style={{fontSize: '40px'}}>
+                <Link style={{color: 'black'}} to="/login">
+                    RockPaperBazooka
+                </Link>
+            </NavbarBrand>
+              <Nav className="ml-auto" navbar>
                 <NavItem>
-                    <h1>RockPaperBazooka</h1>
+                    <Link style={{fontSize: '24px', paddingRight: '50px'}}to="/matches">Matches</Link>
+                </NavItem>
+                <NavItem>
+                    <Link style={{fontSize: '24px', paddingRight: '50px'}}to="/user">User</Link>
                 </NavItem>
               </Nav>
           </Navbar>
